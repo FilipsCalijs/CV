bb6da3c4d5ce769c6f6d0deca763db56242d8bf2
Fil <88441203+FilipsCalijs@users.noreply.github.com>
2024-06-05 20:16:50 +0300

    edit SEO data and start wp server

--- SEO/expiredDomains/expiredDomains/chekcer.py
+++ SEO/expiredDomains/expiredDomains/chekcer.py
@@ -1,23 +1,21 @@
 import os
 import requests
-from bs4 import BeautifulSoup
 from xml.etree import ElementTree
 import datetime
-import shutil
-import subprocess
 
-# Function to read domains from file
+# Функция для чтения доменов из файла
 def read_domains(file_path):
     with open(file_path, 'r') as file:
-        return [domain.strip() for domain in file.readlines()]
+        domains = file.readlines()
+    return [domain.strip() for domain in domains]
 
-# Function to write domains to file
-def write_domains(domains, output_file):
+# Функция для записи доменов с новостями в файл
+def write_domains_with_news(domains, output_file):
     with open(output_file, 'w') as file:
         for domain in domains:
             file.write(f"{domain}\n")
 
-# Function to check for news related to domain
+# Функция для проверки наличия новостей по домену
 def check_domain_for_news(domain):
     base_url = "http://xmlriver.com/search/xml"
     params = {
@@ -26,101 +24,58 @@ def check_domain_for_news(domain):
         "key": "5b7e4d6a8fdc9130b39487dca999b99ba4fb1b08",
         "query": f"site:{domain}"
     }
-    try:
-        response = requests.get(base_url, params=params)
-        response.raise_for_status()
-        xml_root = ElementTree.fromstring(response.content)
-        return xml_root.find('.//results') is not None
-    except (requests.RequestException, ElementTree.ParseError) as e:
-        print(f"Error checking news for domain {domain}: {e}")
+    response = requests.get(base_url, params=params)
+    if response.status_code == 200:
+        try:
+            xml_root = ElementTree.fromstring(response.content)
+            if xml_root.find('.//results') is not None:
+                return True
+            else:
+                return False
+        except ElementTree.ParseError:
+            return False
+    else:
         return False
 
-# Function to get first indexing date from Google
-def get_first_indexing_date(domain):
-    url = f'https://www.google.com/search?q=About+https://{domain}/&tbm=ilp'
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'
-    }
-    try:
-        response = requests.get(url, headers=headers)
-        response.raise_for_status()
-        soup = BeautifulSoup(response.text, 'html.parser')
-        elements = soup.find_all(attrs={"jsrenderer": "ndJhy"})
-        for element in elements:
-            if "Site first indexed by Google" in element.text:
-                return element.text.replace("Site first indexed by Google", "").strip()
-    except requests.RequestException as e:
-        print(f"Error getting indexing date for domain {domain}: {e}")
-    return None
-
-# Function to find common domains
-def find_common_domains(file1, file2):
-    domains1 = set(read_domains(file1))
-    domains2 = set(read_domains(file2))
-    return list(domains1 & domains2)
-
 def main():
-    input_file = 'expiredDomains.txt'
-    output_file_with_date = '1_OldImg.txt'
-    output_file_with_news = '2_HaveNews.txt'
-    common_domains_output_file = '3_both.txt'
+    input_file = r'B:\promma\SEO\HowOldSite\expiredDomains\expiredDomains.txt'
+    output_file_with_news = r'B:\promma\SEO\HowOldSite\expiredDomains\have_news.txt'
     output_txt_file = 'output.txt'
-    target_directory = r'B:\promma\SEO\HowOldSite\probeli'
-    target_domains_file = os.path.join(target_directory, 'domains.txt')
-    script_to_run = os.path.join(target_directory, 'script.py')
 
-    # Delete output.txt if it exists
-    if os.path.exists(output_txt_file):
-        os.remove(output_txt_file)
-        print(f"{output_txt_file} deleted.")
-
+    # Чтение доменов из файла
     domains = read_domains(input_file)
+
+    print("Идет загрузка...")
+
     total_domains = len(domains)
-    domains_with_news = []
-    domains_with_date = []
+    domains_with_news = 0
     domains_without_news = []
 
-    print("Processing...")
+    # Получаем текущую дату для имени файла
+    current_date = datetime.datetime.now().strftime("%Y-%m-%d")
 
-    for index, domain in enumerate(domains):
-        if not domain:
-            continue
-        
-        if check_domain_for_news(domain):
-            domains_with_news.append(domain)
-            print(f"{domain} - Have News")
-        else:
-            domains_without_news.append(domain)
-            print(f"{domain} - null")
+    with open(output_file_with_news, 'w') as output_file_news:
+        for index, domain in enumerate(domains):
+            if domain and domain not in domains_without_news:
+                if check_domain_for_news(domain):
+                    domains_with_news += 1
+                    output_file_news.write(f"{domain}\n")
+                    print(f"{domain} - Have News")
+                else:
+                    domains_without_news.append(domain)
+                    print(f"{domain} - null")
 
-        indexing_date = get_first_indexing_date(domain)
-        if indexing_date:
-            domains_with_date.append(domain)
-            print(f"First indexing date found for {domain}: {indexing_date}")
-        
-        print(f"Processed {index + 1} of {total_domains}. Found {len(domains_with_news)} domains with news.")
+            # Выводим информацию о прогрессе обработки
+            print(f"Обработано {index + 1} из {total_domains}. Найдено {domains_with_news} доменов с новостями.")
 
-    write_domains(domains_with_date, output_file_with_date)
-    write_domains(domains_with_news, output_file_with_news)
+    print(f"Общее количество проверенных доменов: {total_domains}")
+    print(f"Количество доменов с новостями: {domains_with_news}")
 
-    common_domains = find_common_domains(output_file_with_date, output_file_with_news)
-    write_domains(common_domains, common_domains_output_file)
-    write_domains(common_domains, output_txt_file)
-
-    print(f"Total checked domains: {total_domains}")
-    print(f"Domains with news: {len(domains_with_news)}")
-    print(f"Common domains: {len(common_domains)}")
-
-    # Move 3_both.txt to the target directory and overwrite domains.txt
-    shutil.copy(common_domains_output_file, target_domains_file)
-    print(f"Copied {common_domains_output_file} to {target_domains_file}")
-
-    # Run script.py in the target directory
-    try:
-        subprocess.run(['python', script_to_run], check=True)
-        print(f"Ran {script_to_run} successfully.")
-    except subprocess.CalledProcessError as e:
-        print(f"Error running script: {e}")
+    # Чтение доменов с новостями для записи в output.txt
+    domains_for_output_txt = read_domains(output_file_with_news)
+    with open(output_txt_file, 'w') as file:
+        for domain in domains_for_output_txt:
+            file.write(f"{domain}\n")
 
 if __name__ == "__main__":
     main()
